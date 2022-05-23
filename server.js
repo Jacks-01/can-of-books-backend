d72b56a6b9aa8e3d407e99349eb76910d969f1be
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-23 16:30:53 -0500

    setup server.js modeling demo code

--- server.js
+++ server.js
@@ -3,15 +3,43 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Book = require('./models/book');
 
 const app = express();
 app.use(cors());
-
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
 
-  response.send('test request received');
+// Connect to mongoose
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error(`DB ERROR: ${err}`);
+})
+db.once('open', () => {
+  console.log(`Connected to database`);
+});
+
+// Define the end point for books
+app.get('/books', async (req, res) => {
+  console.log(`request for books`);
+  const filterQuery = {};
+  if (req.query.location) {
+    filterQuery.location = req.query.location;
+
+  }
+  const books = await Books.find(filterQuery);
+  res.send(books);
+
+
+});
+
+
+app.get('/test', (req, res) => {
+
+  req.send('test request received');
 
 });
 
